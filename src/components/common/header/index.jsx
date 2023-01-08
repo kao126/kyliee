2086cadae6a53baceebf0215ed2fc500da561c7f
kao126 <76607249+kao126@users.noreply.github.com>
2023-01-09 01:21:59 +0900

    ヘッダーとフッターの構成を変更

---
+++ src/components/common/header/index.jsx
@@ -0,0 +1,16 @@
+
+// style
+import styles from "src/components/common/header/header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>qqq</div>
+      <ul className={styles.ul}>
+        <li className={styles.li}>aaa</li>
+        <li className={styles.li}>aaa</li>
+        <li className={styles.li}>aaa</li>
+      </ul>
+    </header>
+  )
+}
