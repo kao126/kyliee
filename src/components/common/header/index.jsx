539a22012f1a558a9d8861b6402e87bc87447fad
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-09 22:43:41 +0900

    ヘッダーの構成を変更

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -5,11 +5,12 @@ import styles from "src/components/common/header/header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>qqq</div>
+      <div className={styles.logo}>logo</div>
       <ul className={styles.ul}>
-        <li className={styles.li}>aaa</li>
-        <li className={styles.li}>aaa</li>
-        <li className={styles.li}>aaa</li>
+        <li className={styles.li}>About</li>
+        <li className={styles.li}>News</li>
+        <li className={styles.li}>Cart</li>
+        <li className={styles.li}>Menu</li>
       </ul>
     </header>
   )
