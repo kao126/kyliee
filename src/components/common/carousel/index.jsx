23dda818672923fafcc849ada5b0c314d81aa35d
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-12 22:53:00 +0900

    carouselの実装。Autoplayまで

--- src/components/common/carousel/index.jsx
+++ src/components/common/carousel/index.jsx
@@ -1,45 +1,46 @@
 import Image from 'next/image'
-import Test1 from 'public/images/test1.jpg'
-import Test2 from 'public/images/test2.jpg'
-import Test3 from 'public/images/test3.jpg'
-
 // import Swiper core and required modules
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import styles from 'src/components/common/carousel/carousel.module.css'
 
-const images = [ Test2, Test3 ]
+import Test1 from 'public/images/test1.jpg'
+import Test2 from 'public/images/test2.jpg'
+import Test3 from 'public/images/test3.jpg'
+const images = [Test1, Test2, Test3 ]
 
 export function Carousel() {
-
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      modules={[Navigation, Pagination, Scrollbar, Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
-      // scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      autoplay= {{ delay: 3000 }}
-      loop= {true}
+      scrollbar={{ draggable: true }}
+      autoplay={{
+        delay: 4000
+      }}
+      loop={true}
     >
-      {images.map(image => {
+      {images.map((image, i) => {
         return (
-          <SwiperSlide key={image}>
-            <Image
-              src={image}
-              width={1280}
-              alt="test_image"
-              />
-              {console.log(image)}
+          <SwiperSlide key={i}>
+            <div className='image-wrapper'>
+              <Image
+                src={image}
+                alt="test_image"
+                width={1280}
+                height={600}
+                className={styles.test-image}
+                />
+              </div>
           </SwiperSlide>
         )
       })}
