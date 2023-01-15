ba3d9ff4eac704da8f15ea0f1f5bfe5209c8411d
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-15 21:10:06 +0900

    トップカルーセルの調整

--- src/components/common/carousel/index.jsx
+++ src/components/common/carousel/index.jsx
@@ -1,27 +1,26 @@
 import Image from 'next/image'
 // import Swiper core and required modules
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+import { Pagination, Scrollbar, Autoplay } from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import styles from 'src/components/common/carousel/carousel.module.css'
 
-import Test1 from 'public/images/test1.jpg'
-import Test2 from 'public/images/test2.jpg'
-import Test3 from 'public/images/test3.jpg'
-const images = [Test1, Test2, Test3 ]
+import Image1 from 'public/images/test1.jpg'
+import Image2 from 'public/images/test2.jpg'
+import Image3 from 'public/images/test3.jpg'
+
+const images = [Image1, Image2, Image3 ]
 
 export function Carousel() {
   return (
     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+      modules={[Pagination, Scrollbar, Autoplay]}
       spaceBetween={50}
       slidesPerView={1}
-      navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       autoplay={{
@@ -32,15 +31,16 @@ export function Carousel() {
       {images.map((image, i) => {
         return (
           <SwiperSlide key={i}>
-            <div className='image-wrapper'>
               <Image
                 src={image}
                 alt="test_image"
-                width={1280}
-                height={600}
-                className={styles.test-image}
+                sizes="(max-width: 768px) 100vw,
+                (max-width: 425px) 100vw,
+                100vw"
+                width={1480}
+                height={780}
+                object-fit="cover"
                 />
-              </div>
           </SwiperSlide>
         )
       })}
