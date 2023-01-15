17f9f326a213d7056da631abe1018a76b8be61f0
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-15 21:10:28 +0900

    トップニュースコンポーネントの作成

--- src/components/top/main.jsx
+++ src/components/top/main.jsx
@@ -1,6 +1,7 @@
 
 // components
 import { Carousel } from "src/components/common/carousel";
+import { TopNews } from "src/components/top/topNews";
 
 // style
 import styles from "src/components/top/main.module.css";
@@ -9,6 +10,7 @@ export function Main() {
   return (
     <main className={styles.main}>
       <Carousel />
+      <TopNews />
     </main>
   )
 }
