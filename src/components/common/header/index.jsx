451db5b8cfa2f04a2d5e92f4a091fdcf578a7636
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-15 21:28:53 +0900

    ロゴ画像をimpot

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,11 +1,19 @@
-
+// Next.js
+import Image from "next/image";
 // style
 import styles from "src/components/common/header/header.module.css";
+
+import Logo from "public/images/kyliee.png"
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>logo</div>
+      <div className={styles.logo}>
+        <Image 
+          src={Logo}
+          height={70}
+        />
+      </div>
       <ul className={styles.ul}>
         <li className={styles.li}>About</li>
         <li className={styles.li}>News</li>
