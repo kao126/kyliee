122c875cc8d7304e5c03ca798bd35fca0e9fde42
kao126 <76607249+kao126@users.noreply.github.com>
2023-01-26 00:18:11 +0900

    コンテントコンポーネントを軽くデザイン

--- src/components/top/topContents.jsx
+++ src/components/top/topContents.jsx
@@ -1,7 +1,7 @@
 // Next.js
 import Image from "next/image";
 // style
-// import styles from "src/components/top/topContents.module.css";
+import styles from "src/components/top/topContents.module.css";
 
 import Image1 from "public/images/test1.jpg";
 import Image2 from "public/images/test2.jpg";
@@ -13,17 +13,37 @@ export function TopContents() {
   return (
     <>
       {images.map((image, i) => {
-        return (
-          <Image
-            src={image}
-            key={i}
-            alt="test_image"
-            width={600}
-            height={450}
-            object-fit="cover"
-            aria-hidden="true"
-          />
-        );
+        if (i % 2 === 0) {
+          return (
+            <div className={styles.contents_wrapper}>
+              <Image
+                src={image}
+                key={i}
+                alt="test_image"
+                width={600}
+                height={450}
+                object-fit="cover"
+                aria-hidden="true"
+              />
+              <div className={styles.contents_description}>あああ</div>
+            </div>
+          );
+        } else {
+          return (
+            <div className={styles.contents_wrapper}>
+              <div className={styles.contents_description}>あああ</div>
+              <Image
+                src={image}
+                key={i}
+                alt="test_image"
+                width={600}
+                height={450}
+                object-fit="cover"
+                aria-hidden="true"
+              />
+            </div>
+          );
+        }
       })}
     </>
   );
