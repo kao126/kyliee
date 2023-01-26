038cd7f18f0ae09d4a469a4874f95b7189210337
kao126 <76607249+kao126@users.noreply.github.com>
2023-01-26 23:13:50 +0900

    お知らせ部分をスタイリング

--- src/components/top/topNews.jsx
+++ src/components/top/topNews.jsx
@@ -3,10 +3,11 @@ import styles from "src/components/top/topNews.module.css";
 
 export function TopNews() {
   return (
-    <div>
+    <div className={styles.top_news_wrapper}>
       <div className={styles.title_wrapper}>
-      <h2>News</h2>
-      <div className={styles.news_link}>過去の一覧</div>
+        <div></div>
+        <h1>News</h1>
+        <div className={styles.news_link}>過去の一覧</div>
       </div>
       <ul className={styles.contents_wrapper}>
         <li>News Contents</li>
@@ -14,5 +15,5 @@ export function TopNews() {
         <li>News Contents</li>
       </ul>
     </div>
-  )
+  );
 }
