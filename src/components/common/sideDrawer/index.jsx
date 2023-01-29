31b8ed54d7ea02f1a8095ee757913f9647cb70ad
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-29 18:52:33 +0900

    ハンバーガーメニューを実装　枠外でも閉じられるように変更

---
+++ src/components/common/sideDrawer/index.jsx
@@ -0,0 +1,29 @@
+// Material-Ui
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export function SideDrawer({ open, handleDrawer }) {
+  const drawerWidth = 240;
+
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+        },
+      }}
+      anchor="right"
+      onClose={handleDrawer}
+      open={open}
+    >
+      {/* <DrawerHeader> */}
+      <div>aaa</div>
+      <div>aaa</div>
+      <div>aaa</div>
+      {/* </DrawerHeader> */}
+    </Drawer>
+  );
+}
