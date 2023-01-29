bc4e163f258ff3f849958005a97e7895bf294d0d
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-01-29 18:53:16 +0900

    エントリーのloadingページを作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
 // Next.js
 import Head from "next/head";
 
+// React
+import { useEffect, useState } from "react";
+
 // components
 import { Header } from "src/components/common/header";
 import { Main } from "src/components/top/main";
@@ -14,15 +17,30 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const { isHeaderActive } = useHeaderScroll();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2 * 1000);
+  }, []);
 
   return (
     <>
       <Head>
         <title>Top</title>
       </Head>
-      <Header isActive={isHeaderActive} />
-      <Main />
-      <Footer />
+      {isLoading ? (
+        <div className={styles.entry_loading}>
+          <div className={styles.loading_border}></div>
+        </div>
+      ) : (
+        <div className={styles.top}>
+          <Header isActive={isHeaderActive} />
+          <Main />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
