b0801571f22bebee47800b642e8239c274da90cd
kao126 <76607249+kao126@users.noreply.github.com>
2023-01-29 18:52:33 +0900

    ハンバーガーメニューを実装　枠外でも閉じられるように変更

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -4,24 +4,22 @@ import { useState } from "react";
 // Next.js
 import Image from "next/image";
 
+// components
+import { SideDrawer } from "src/components/common/sideDrawer";
+
 // style
 import styles from "src/components/common/header/header.module.css";
 import Logo from "public/images/kyliee.png";
 
 // Material-Ui
-import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
 export function Header({ isActive }) {
   const isDisplay = isActive ? styles.is_display : styles.is_not_display;
-  const drawerWidth = 240;
   const [open, setOpen] = useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const handleDrawer = () => {
+    setOpen(!open);
   };
 
   return (
@@ -31,32 +29,15 @@ export function Header({ isActive }) {
           <Image src={Logo} alt="logo" height={70} />
         </div>
         <ul className={styles.ul}>
-          <li className={styles.li}>About</li>
-          <li className={styles.li}>News</li>
-          <li className={styles.li}>Cart</li>
-          <li className={styles.li}>
-            <IconButton color="inherit" aria-label="open drawer" edge="end" onClick={handleDrawerOpen} sx={{ ...(open && { display: "none" }) }}>
-              <MenuIcon />
-            </IconButton>
-          </li>
+          <li>About</li>
+          <li>News</li>
+          <li>Cart</li>
+          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawer}>
+            <MenuIcon fontSize="large" />
+          </IconButton>
         </ul>
       </header>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-          },
-        }}
-        variant="persistent"
-        anchor="right"
-        open={open}
-      >
-        {/* <DrawerHeader> */}
-        <IconButton onClick={handleDrawerClose}></IconButton>
-        {/* </DrawerHeader> */}
-      </Drawer>
+      <SideDrawer handleDrawer={handleDrawer} open={open} />
     </>
   );
 }
