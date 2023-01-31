69ff2bcbc42eafd56f62b9dc6561e96ad15d5f55
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-02-01 00:04:35 +0900

    topColorコンポーネントにCardを追加

--- src/components/top/topColor.jsx
+++ src/components/top/topColor.jsx
@@ -1,13 +1,19 @@
 // style
 import styles from "src/components/top/topColor.module.css";
 
+// Material-Ui
+import { Card } from "@mui/material";
+
 export function TopColor() {
   return (
     <div>
       <h1 className={styles.title}>topColor</h1>
+      <h4 className={styles.description}>トップカラーの説明</h4>
       <div className={styles.color_wrapper}>
-        <div className={styles.color_contents}>Red</div>
-        <div className={styles.color_contents}>Blue</div>
+        <Card className={styles.color_contents}>Red</Card>
+        <Card className={styles.color_contents}>Blue</Card>
+        {/* <div className={styles.color_contents}>Red</div>
+        <div className={styles.color_contents}>Blue</div> */}
       </div>
     </div>
   );
