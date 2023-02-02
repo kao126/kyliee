af9ae6f17f013314477f8928fbbb58ebddaddea7
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-02-02 23:38:38 +0900

    styledコンポーネントを利用して色付け（仮）

--- src/components/top/topColor.jsx
+++ src/components/top/topColor.jsx
@@ -1,20 +1,59 @@
 // style
-import styles from "src/components/top/topColor.module.css";
+import styled from '@emotion/styled';
 
 // Material-Ui
-import { Card } from "@mui/material";
+import {Card} from '@mui/material';
+import {red, indigo} from '@mui/material/colors';
+import {flexbox} from '@mui/system';
+
+const red_bgcolor = red[50];
+const red_border = red[400];
+
+const indigo_bgcolor = indigo[50];
+const indigo_border = indigo[400];
 
 export function TopColor() {
   return (
-    <div>
-      <h1 className={styles.title}>topColor</h1>
-      <h4 className={styles.description}>トップカラーの説明</h4>
-      <div className={styles.color_wrapper}>
-        <Card className={styles.color_contents}>Red</Card>
-        <Card className={styles.color_contents}>Blue</Card>
-        {/* <div className={styles.color_contents}>Red</div>
-        <div className={styles.color_contents}>Blue</div> */}
+    <StyledWrapper>
+      <h1 className='title'>topColor</h1>
+      <h4 className='description'>トップカラーの説明</h4>
+      <div className='color_wrapper'>
+        <StyledCard>Red</StyledCard>
+        <StyledCard sx={{bgcolor: indigo_bgcolor, border: 1.5, borderColor: indigo_border}}>Blue</StyledCard>
       </div>
-    </div>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled('div')`
+  .title {
+    text-align: center;
+    border-bottom: 3px solid #ccc;
+    margin-bottom: 1rem;
+  }
+  .description {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  .color_wrapper {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+    width: 80%;
+    margin: 0 auto;
+  }
+`;
+
+const StyledCard = styled(Card)`
+  width: 40%;
+  height: 240px;
+  /* border: 3px solid #ccc; */
+  border-radius: 15px;
+  background-color: red_bgcolor;
+  border: ${({red_bgcolor}) => red_border} 1.5 solid;
+  font-size: 36px;
+  color: red_border;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
