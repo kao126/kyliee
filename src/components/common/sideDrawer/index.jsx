9036b9a03c348d5eb13693af1986b2b330a3b978
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-02-12 01:02:57 +0900

    drawerのレイアウトを変更

--- src/components/common/sideDrawer/index.jsx
+++ src/components/common/sideDrawer/index.jsx
@@ -1,29 +1,36 @@
 // Material-Ui
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import styled from '@emotion/styled';
+import {List} from '@mui/material';
+import Link from 'next/link';
 
-export function SideDrawer({ open, handleDrawer }) {
-  const drawerWidth = 300;
-
+export function SideDrawer({open, handleDrawer}) {
   return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-        },
-      }}
-      anchor="right"
-      onClose={handleDrawer}
-      open={open}
-    >
+    <StyledDrawer anchor='right' onClose={handleDrawer} open={open}>
       {/* <DrawerHeader> */}
-      <div>aaa</div>
-      <div>aaa</div>
-      <div>aaa</div>
+      <StyledList>
+        <div>aaa</div>
+        <div><Link href={'/about'}>About</Link></div>
+        <div><Link href={'/concept'}>Concept</Link></div>
+        <div>aaa</div>
+      </StyledList>
       {/* </DrawerHeader> */}
-    </Drawer>
+    </StyledDrawer>
   );
 }
+const StyledDrawer = styled(Drawer)`
+  .MuiDrawer-paper {
+    width: 470px;
+  }
+`;
+
+const StyledList = styled(List)`
+  padding-top: 130px;
+  margin: 0 85px;
+  text-align: center;
+  > div {
+    margin-bottom: 33px;
+  }
+`;
