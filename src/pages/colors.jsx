9c430f22dd576316332be24c5e63b964aa1f3617
kao126 <76607249+kao126@users.noreply.github.com>
2023-03-05 23:47:53 +0900

    colorページにモーダルを追加して、pickerの値がフォームとboxに反映されるように実装

--- src/pages/colors.jsx
+++ src/pages/colors.jsx
@@ -1,22 +1,40 @@
+import React, {useState} from 'react';
+import {SketchPicker} from 'react-color';
 import Head from 'next/head';
-import Image from 'next/image';
 import {Inter} from '@next/font/google';
 
 // style
-// import "../styles/globals.css";
-import styles from 'src/styles/Home.module.css';
+import styled from '@emotion/styled';
+import theme from 'src/theme';
 
 // components
 import {Header} from 'src/components/common/header';
 import {Footer} from 'src/components/common/footer';
-import styled from '@emotion/styled';
 
 // material-ui
-import { Box, TextField } from '@mui/material';
+import {Box, Grid, Modal, TextField} from '@mui/material';
 
 const inter = Inter({subsets: ['latin']});
 
 export default function Colors() {
+  const [firstColor, setFirstColor] = useState('#fff');
+  const [secondColor, setSecondColor] = useState('#fff');
+  const [firstPicker, setFirstPicker] = useState(false);
+  const [secondPicker, setSecondPicker] = useState(false);
+
+  const handleFirstChange = (color) => {
+    setFirstColor(color.hex);
+  };
+  const handleSecondChange = (color) => {
+    setSecondColor(color.hex);
+  };
+
+  const handleFirstPicker = () => {
+    setFirstPicker((prevPicker) => !prevPicker);
+  };
+  const handleSecondPicker = () => {
+    setSecondPicker((prevPicker) => !prevPicker);
+  };
   return (
     <>
       <Head>
@@ -25,20 +43,48 @@ export default function Colors() {
       <Header />
       <StyledMain>
         <h2>Color Palette</h2>
-
-        <TextField id='standard-basic' label='color 1' variant='standard' />
-        <TextField id='standard-basic' label='color 2' variant='standard' />
-
-        <Box className="color-palette"></Box>
+        <TextField id='standard-basic' label='color 1' variant='standard' value={firstColor} onClick={handleFirstPicker} />
+        <TextField id='standard-basic' label='color 2' variant='standard' value={secondColor} onClick={handleSecondPicker} />
+        <Modal open={firstPicker} onClose={handleFirstPicker}>
+          <div>
+            <h4>Color1</h4>
+            <SketchPicker color={firstColor} onChange={handleFirstChange} />
+          </div>
+        </Modal>
+        <Modal open={secondPicker} onClose={handleSecondPicker}>
+          <div>
+            <h4>Color2</h4>
+            <SketchPicker color={secondColor} onChange={handleSecondChange} />{' '}
+          </div>
+        </Modal>
+        <Grid container>
+          <Grid item>
+            <Box
+              className='color-palette'
+              style={{
+                backgroundColor: firstColor,
+              }}
+            ></Box>
+            {firstColor}
+          </Grid>
+          <Grid item>
+            <Box
+              className='color-palette'
+              style={{
+                backgroundColor: secondColor,
+              }}
+            ></Box>
+            {secondColor}
+          </Grid>
+        </Grid>
       </StyledMain>
     </>
   );
 }
 
 const StyledMain = styled('main')`
-.color-palette {
-  width: 80px;
-  height: 80px;
-  background-color: blue;
-}
+  .color-palette {
+    width: 80px;
+    height: 80px;
+  }
 `;
