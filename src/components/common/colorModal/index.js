934bbbef347bf05734b56ea33c9c72e76899b6e7
kao126 <76607249+kao126@users.noreply.github.com>
2023-03-11 18:57:24 +0900

    カラーページのカラーパレットをコンポーネント化

---
+++ src/components/common/colorModal/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SketchPicker} from 'react-color';
+import styled from '@emotion/styled';
+import {Modal} from '@mui/material';
+
+const ColorModal = ({open, onClose, color, onChange, title}) => {
+  return (
+    <StyledModal open={open} onClose={onClose}>
+      <div className='picker-contents'>
+        <h4>{title}</h4>
+        <SketchPicker color={color} onChange={onChange} />
+      </div>
+    </StyledModal>
+  );
+};
+
+const StyledModal = styled(Modal)`
+  .picker-contents {
+    width: fit-content;
+    margin: 0 auto;
+  }
+`;
+
+export default ColorModal;
