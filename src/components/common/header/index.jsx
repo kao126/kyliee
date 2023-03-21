b0162aefac45c207ae5489bcb9f5d3792aa48c35
kao126 <76607249+kao126@users.noreply.github.com>
2023-03-21 19:47:27 +0900

    headerのcss形式をstyled-componentに変更して、レイアウトを変更

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,46 +1,67 @@
 // React
-import {useState} from 'react';
+import { useState } from 'react';
 
 // Next.js
 import Image from 'next/image';
 import Link from 'next/link';
 
 // components
-import {SideDrawer} from 'src/components/common/sideDrawer';
+import { SideDrawer } from 'src/components/common/sideDrawer';
 
 // Hooks
 import { useSideDrawer } from 'src/hooks/useSideDrawer';
 
 // style
-import styles from 'src/components/common/header/header.module.css';
+import styled from '@emotion/styled';
 import Logo from 'public/images/kyliee.png';
 
 // Material-Ui
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export function Header({isActive}) {
-  const isDisplay = isActive ? styles.is_display : styles.is_not_display;
-  const {open, handleDrawer} = useSideDrawer();
+export function Header({ isActive }) {
+  const isDisplay = isActive ? 'is_display' : 'is_not_display';
+  const { open, handleDrawer } = useSideDrawer();
 
   return (
     <>
-      <header className={`${styles.header} ${isDisplay}`}>
-        <div className={styles.logo}>
+      <StyledHeader className={isDisplay}>
+        <div className='logo'>
           <Image src={Logo} alt='logo' height={70} />
         </div>
-        <ul className={styles.ul}>
-          <li>About</li>
-          <li>
-            <Link href={'/#news'}>News</Link>
-          </li>
-          <li>Cart</li>
-          <IconButton color='inherit' aria-label='open drawer' onClick={handleDrawer}>
-            <MenuIcon fontSize='large' />
-          </IconButton>
-        </ul>
-      </header>
+        <IconButton color='inherit' aria-label='open drawer' onClick={handleDrawer}>
+          <MenuIcon className='menu-icon' />
+        </IconButton>
+      </StyledHeader>
       <SideDrawer handleDrawer={handleDrawer} open={open} />
     </>
   );
 }
+
+const StyledHeader = styled('header')`
+  width: 100%;
+  height: 90px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 20px;
+  position: fixed;
+  z-index: 9999;
+  .is_display {
+    opacity: 1;
+  }
+
+  .is_not_display {
+    opacity: 0;
+  }
+
+  .logo {
+    width: 15%;
+    align-self: flex-end;
+  }
+  .menu-icon {
+    font-size: 3rem;
+    color: #333;
+  }
+`;
