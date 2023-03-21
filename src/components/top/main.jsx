209f258e73de835502efdaf4fd1b63eb6a721f20
kao126 <76607249+kao126@users.noreply.github.com>
2023-03-21 19:20:38 +0900

    SNSの一覧を作成

--- src/components/top/main.jsx
+++ src/components/top/main.jsx
@@ -8,9 +8,28 @@ import { TopColor } from 'src/components/top/topColor';
 import styled from '@emotion/styled';
 import theme from 'src/theme';
 
+// material-ui
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 export function Main() {
   return (
     <StyledMain>
+      <div className='sns-area'>
+        <p className='sns-title'>SNS</p>
+        <ul className='sns-list'>
+          <li className='sns-contents'>
+            <FacebookRoundedIcon />
+          </li>
+          <li className='sns-contents'>
+            <InstagramIcon />
+          </li>
+          <li className='sns-contents'>
+            <TwitterIcon />
+          </li>
+        </ul>
+      </div>
       <Carousel />
       <StyledContainer>
         <TopNews />
@@ -23,6 +42,37 @@ export function Main() {
 
 const StyledMain = styled('main')`
   background-color: ${theme.palette.background.main};
+  .sns-area {
+    position: fixed;
+    bottom: 120px;
+    right: 35px;
+    z-index: 9999;
+    .sns-title {
+      writing-mode: vertical-rl;
+      font-size: 1.1rem;
+      position: relative;
+      letter-spacing: 0.08em;
+      margin: 0;
+      :before {
+        content: '';
+        position: absolute;
+        bottom: -65px;
+        right: 13px;
+        width: 1px;
+        height: 45px;
+        background-color: #222;
+      }
+    }
+    .sns-list {
+      text-align: center;
+      margin-top: 75px;
+      list-style: none;
+      padding: 0;
+    }
+    .sns-contents {
+      padding: 5px 0;
+    }
+  }
 `;
 
 const StyledContainer = styled('div')`
