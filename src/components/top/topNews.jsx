20cd77917575430bd430cd9d44f183b32665ffe5
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-03-23 00:02:11 +0900

    topNewsのレイアウト形式をstyled-componentsに変更

--- src/components/top/topNews.jsx
+++ src/components/top/topNews.jsx
@@ -1,19 +1,57 @@
 // style
-import styles from 'src/components/top/topNews.module.css';
+import styled from '@emotion/styled';
 
 export function TopNews() {
   return (
-    <div id='news' className={styles.top_news_wrapper}>
-      <div className={styles.title_wrapper}>
+    <StyledNews id='news'>
+      <div className='title_wrapper'>
         <div></div>
         <h1>News</h1>
-        <div className={styles.news_link}>過去の一覧</div>
+        <div className='news_link'>MORE</div>
       </div>
-      <ul className={styles.contents_wrapper}>
+      <ul className='contents_wrapper'>
         <li>News Contents</li>
         <li>News Contents</li>
         <li>News Contents</li>
       </ul>
-    </div>
+    </StyledNews>
   );
 }
+
+const StyledNews = styled('div')`
+  margin: 50px 0 100px;
+  .title_wrapper {
+    display: flex;
+    margin-top: 30px;
+    border-bottom: 3px solid #ccc;
+    justify-content: space-between;
+  }
+  .news_link {
+    align-self: end;
+    position: relative;
+    ::before {
+      content: '';
+      position: absolute;
+      width: 35px;
+      height: 1px;
+      right: 55px;
+      top: 12px;
+      background-color: #222;
+    }
+    ::after {
+      content: '>';
+      position: absolute;
+      right: 55px;
+      color: #222;
+    }
+  }
+  .contents_wrapper {
+    width: 90%;
+    margin: 0 auto;
+    padding-top: 20px;
+  }
+
+  .contents_wrapper > li {
+    margin-bottom: 10px;
+  }
+`;
