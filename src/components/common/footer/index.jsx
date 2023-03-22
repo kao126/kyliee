c8e9d1406e5c94912f6f85d5f91b1a83f091aae5
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-03-23 00:02:43 +0900

    conceptページを少し変更フッター作成も着手

--- src/components/common/footer/index.jsx
+++ src/components/common/footer/index.jsx
@@ -1,7 +1,14 @@
+import styled from '@emotion/styled';
 import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export function Footer() {
-  return <footer>Footer</footer>;
+  return <StyledFooter>Footer</StyledFooter>;
 }
+
+const StyledFooter = styled('footer')`
+  position: fixed;
+  bottom: 10px;
+  color: #646464;
+`;
