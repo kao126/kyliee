3ee0c47723dccb1d1a18d7ae94c55504e6dec3e7
kao126 <76607249+kao126@users.noreply.github.com>
2023-04-02 18:06:17 +0900

    サイドドロワーをレイアウト

--- src/components/common/sideDrawer/index.jsx
+++ src/components/common/sideDrawer/index.jsx
@@ -1,44 +1,93 @@
-// Material-Ui
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import styled from '@emotion/styled';
-import { List } from '@mui/material';
 import Link from 'next/link';
+
+// styles
+import styled from '@emotion/styled';
+
+// Material-Ui
+import { Drawer, Button, List, ListItem } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export function SideDrawer({ open, handleDrawer }) {
   return (
     <StyledDrawer anchor='right' onClose={handleDrawer} open={open}>
-      {/* <DrawerHeader> */}
+      <StyledButton onClick={handleDrawer}>
+        <CloseIcon className='close_icon' />
+      </StyledButton>
       <StyledList>
-        <div>
-          <Link href={'/'}>Top</Link>
-        </div>
-        <div>
-          <Link href={'/about'}>About</Link>
-        </div>
-        <div>
-          <Link href={'/concept'}>Concept</Link>
-        </div>
-        <div>
-          <Link href={'/gallery'}>Gallery</Link>
-        </div>
+        <ListItem disablePadding>
+          <Link href={'/'}>
+            Top<span>トップ</span>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href={'/about'}>
+            About<span>アバウト</span>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href={'/concept'}>
+            Concept<span>コンセプト</span>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href={'/gallery'}>
+            Gallery<span>ギャラリー</span>
+          </Link>
+        </ListItem>
       </StyledList>
-      {/* </DrawerHeader> */}
     </StyledDrawer>
   );
 }
 const StyledDrawer = styled(Drawer)`
   .MuiDrawer-paper {
-    width: 470px;
+    max-width: 400px;
+    width: 66%;
+    justify-content: center;
+    padding: 0 60px;
+  }
+  @media screen and (max-width: 425px) {
+    .MuiDrawer-paper {
+      width: 75%;
+      padding: 0 20px;
+    }
+  }
+`;
+
+const StyledButton = styled(Button)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 12px;
+  z-index: 1;
+  :hover {
+    opacity: 0.6;
+  }
+  .close_icon {
+    font-size: 3rem;
+    color: #333333;
   }
 `;
 
 const StyledList = styled(List)`
-  padding-top: 130px;
-  margin: 0 85px;
+  padding: 40px 20px;
   text-align: center;
-  > div {
-    margin-bottom: 33px;
+  > li {
+    margin-bottom: 25px;
+    > a {
+      color: #333333;
+      font-size: 24px;
+      letter-spacing: 0.03em;
+      text-decoration: none;
+      :hover {
+        opacity: 0.6;
+      }
+      > span {
+        font-size: 16px;
+        margin-left: 13px;
+      }
+    }
+  }
+  @media screen and (max-width: 425px) {
+    padding: 30px 20px;
   }
 `;
