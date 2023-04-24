b5388a6df023c054c0e2f4b36f649226a80a96cb
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-04-24 23:54:03 +0900

    style: layout topBlog comp

--- src/components/top/topBlog.jsx
+++ src/components/top/topBlog.jsx
@@ -87,10 +87,29 @@ const StyledWrapper = styled('div')`
       position: absolute;
       content: '';
       background-color: #fff;
-      width: 30px;
-      height: 5px;
-      top: -5px;
-      right: 35px;
+      width: 16px;
+      height: 1px;
+      top: -9.5px;
+      right: 30px;
+      z-index: 1;
+    }
+    ::after {
+      position: absolute;
+      content: '▷';
+      font-size: 20px;
+      color: #fff;
+      top: -23px;
+      right: 17px;
+      z-index: 1;
+    }
+    /* ::before {
+      position: absolute;
+      content: '';
+      background-color: #fff;
+      width: 15px;
+      height: 1px;
+      top: -9px;
+      right: 30px;
       z-index: 1;
     }
     ::after {
@@ -98,13 +117,22 @@ const StyledWrapper = styled('div')`
       content: '';
       width: 0;
       height: 0;
-      top: -20px;
-      right: 20px;
+      top: -14px;
+      right: 21px;
       z-index: 1;
       border-style: solid;
-      border-width: 16.5px 0px 16.5px 22.5px;
+      border-width: 5.5px 0px 5.5px 7.5px;
       border-color: transparent transparent transparent #fff;
-    }
+    } */
+    /* ::before {
+      position: absolute;
+      content: '▷';
+      color: #fff;
+      font-size: 28px;
+      top: -28px;
+      right: 16px;
+      z-index: 1;
+    } */
   }
   .link {
     position: relative;
@@ -114,9 +142,9 @@ const StyledWrapper = styled('div')`
       position: absolute;
       content: '';
       display: block;
-      width: 70px;
-      height: 70px;
-      top: 25px;
+      width: 50px;
+      height: 50px;
+      top: 30px;
       right: 7px;
       background-color: #333;
       border-radius: 50%;
@@ -129,9 +157,36 @@ const StyledLink = styled(Link)`
   width: 120px;
   color: #333333;
   text-decoration: none;
-  text-align: right;
+  text-align: center;
   right: 0;
+  left: 0;
+  margin: auto;
   font-size: 1rem;
   padding-bottom: 5px;
   border-bottom: 1px solid #abbcc1;
+  transition: 0.3s;
+  overflow: hidden;
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    background: #efefef;
+    transition: transform 0.3s;
+    right: auto;
+    bottom: auto;
+    width: 120%;
+    height: 100%;
+    transform-origin: right top;
+    transform: skewX(-30deg) scale(0, 1);
+  }
+  :hover {
+    ::before {
+      transform-origin: left top;
+      transform: skewX(-30deg) scale(1, 1);
+    }
+  }
 `;
