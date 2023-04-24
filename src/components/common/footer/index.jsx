3c3b9459a87421b37dd01007d8c895929840763d
Kanaru Oshima <kgiy.007ruu@gmail.com>
2023-04-25 00:20:34 +0900

    feat: add LinkedIn icon on footer

--- src/components/common/footer/index.jsx
+++ src/components/common/footer/index.jsx
@@ -3,10 +3,14 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import Image from 'next/image';
 import Link from 'next/link';
 import Wantedly from 'public/images/Wantedly_W.png';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export function Footer() {
   return (
     <StyledFooter>
+      <Link href={'/'}>
+        <LinkedInIcon className='linkedin' />
+      </Link>
       <Link href={'https://github.com/kao126'}>
         <GitHubIcon className='github' />
       </Link>
@@ -26,13 +30,20 @@ const StyledFooter = styled('footer')`
   background-color: #333333;
   .footer_text {
     color: #fff;
+    margin: 0 5px;
   }
   .github {
     color: #fff;
     font-size: 38px;
+    margin: 0 5px;
   }
   .wantedly {
     width: 56px;
     height: auto;
+  }
+  .linkedin {
+    color: #fff;
+    font-size: 42px;
+    margin: 0 5px;
   }
 `;
