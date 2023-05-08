4c3186b2887d4d4d29fbb14350ac74ea7e7f5f63
kao126 <76607249+kao126@users.noreply.github.com>
2023-05-08 23:31:42 +0900

    style: make modal on gallery-page for explanation on my work

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,21 +1,26 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
 
 // components
 import { Header } from 'src/components/common/header';
 import { SimpleFooter } from 'src/components/common/footer/simpleFooter';
 // material-ui
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 // styles
 
 import NoImage from 'public/images/NO_IMAGE.jpg';
 import styled from '@emotion/styled';
-import Link from 'next/link';
+import GalleryModal from 'src/components/gallery/galleryModal';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Gallery() {
+  const [open, setOpen] = useState(false);
+  const handleModal = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
   return (
     <>
       <Head>
@@ -28,34 +33,23 @@ export default function Gallery() {
           <Grid item xs={12} md={6}>
             <div className='item'>
               <img src={NoImage.src} className='img' alt='イメージ画像' />
-              <Link href={'/gallery'} className='gallery'>
+              <Box className='gallery' onClick={handleModal}>
                 <p className='item-title'>タイトル１</p>
-              </Link>
+              </Box>
             </div>
           </Grid>
           <Grid item xs={12} md={6}>
             <div className='item'>
               <img src={NoImage.src} className='img' alt='イメージ画像' />
-              <Link href={'/'} className='gallery'>
+              <Box className='gallery' onClick={handleModal}>
                 <p className='item-title'>タイトル１</p>
-              </Link>
+              </Box>
             </div>
           </Grid>
-          {/* <Grid item xs={12} md={6} lg={4}>
-            <Image src={NoImage} className='img' alt='イメージ画像' />
-            <Typography className='img-description'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur aut hic eius suscipit quasi adipisci earum laboriosam perspiciatis error tempora quae praesentium
-              sit, facere nostrum reprehenderit quaerat, blanditiis fugit culpa.
-            </Typography>
-            <div className='link'>
-              <Link href={'/'} className='more'>
-                more
-              </Link>
-            </div>
-          </Grid> */}
         </Grid>
       </StyledMain>
       <SimpleFooter />
+      <GalleryModal open={open} handleModal={handleModal} />
     </>
   );
 }
@@ -79,16 +73,20 @@ const StyledMain = styled('main')`
       width: 100%;
       height: 100%;
       opacity: 0;
-      :hover {
-        opacity: 1;
-        text-decoration: none;
-        background: rgba(0, 0, 0, 0.5);
-        .item-title {
-          position: absolute;
-          bottom: 30px;
-          left: 50px;
-          color: #fff;
-        }
+    }
+    .item-title {
+      display: none;
+    }
+    .gallery:hover {
+      opacity: 1;
+      text-decoration: none;
+      background: rgba(0, 0, 0, 0.5);
+      .item-title {
+        display: block;
+        position: absolute;
+        bottom: 30px;
+        left: 50px;
+        color: #fff;
       }
     }
   }
@@ -99,31 +97,4 @@ const StyledMain = styled('main')`
     border: thin solid lightgray;
     z-index: -1;
   }
-  /* .img {
-    width: 100%;
-    height: auto;
-    border: thin solid lightgray;
-    margin-bottom: 30px;
-  }
-  .img-description {
-    color: #333;
-    font-size: 14px;
-    margin-bottom: 20px;
-  }
-  .link {
-    text-align: right;
-    .more {
-      display: inline-block;
-      font-size: 14px;
-      color: #333;
-      letter-spacing: 0.1rem;
-      padding: 8px 12px;
-      background-color: #eee;
-      text-decoration: none;
-      text-align: right;
-      :hover {
-        text-decoration: underline;
-      }
-    }
-  } */
 `;
