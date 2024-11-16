06a8f22608cf06c2d6ac964d428dca4bb0dacf6e
kao126 <76607249+kao126@users.noreply.github.com>
2024-11-16 18:42:43 +0900

    update: about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 
 // style
 import styled from '@emotion/styled';
-import selfImage from 'public/images/default-image.jpg';
+import selfImage from 'public/images/about.jpg';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,32 +14,73 @@ export default function About() {
     <>
       <StyledMain>
         {/* スキルについて記載したい。 使用歴と言語 あとは、趣味 経歴 */}
-        <Image src={selfImage} alt='icon' className='img' />
+        <div className='img-wrapper'>
+          <Image src={selfImage} alt='icon' className='img' />
+        </div>
         <div className='contents'>
           <h1>
-            大嶋 捷稔 <span className='en_name'>Kanaru Oshima</span>
+            大嶋 捷稔 <span className='en_name'>Oshima Kanaru</span>
           </h1>
           <div>
             <p className='bold'>経歴</p>
             <p>
-              神奈川県横浜市出身のエンジニア。大学で国際経済学を学んだのち、ICTシステムを開発/販売している会社で営業に従事。その後ITの技術に惹かれ、25才でシステムエンジニアへ転職。事業会社のフルスタックエンジニアとしてキャリアをスタートし、React(Redux),Ruby
-              on Rails,AWS(EC2,S3等)を用いたWebアプリ開発/保守/運用など、インフラからフロントサイドまで幅広く業務を経験してきました。
+              神奈川県横浜市出身のweb系エンジニア。大学で国際経済学を学んだのち、営業としてICTシステムを開発/販売している会社に従事。
+              <br />
+              そこでIT技術に惹かれて、25才でシステムエンジニアに転職。フルスタックエンジニアとしてキャリアを再スタートし、Webアプリ設計/開発/運用/保守など、インフラからフロントサイドまで一貫して幅広く業務を経験。
             </p>
           </div>
-          {/* <div>
-            <p className='bold'>経歴</p>
-            <p>2022年3月末 営業</p>
-            <p>2022年4月~9月 プログラミングスクール & 転職活動</p>
-            <p>2022年10月~ 事業会社 フルスタックエンジニア（現職）</p>
-          </div> */}
           <div>
             <p className='bold'>趣味</p>
-            <p>音楽探し、ジムでの筋トレと柔軟、アニメ鑑賞、ポッドキャストを聴きながら散歩すること</p>
+            <p>株式投資、筋トレ/柔軟、音楽探し、アニメ鑑賞、ポッドキャスト、様々なことにチャレンジすること</p>
           </div>
           <div>
             <p className='bold'>スキル</p>
-            <p>React(Redux), Ruby on Rails(Ruby), AWS, HTML5, CSS3 </p>
-            <p>グラフで書こうかな（習得度などを表せるように）</p>
+            <div className='skill-wrapper'>
+              <ul className='skill'>
+                <li>
+                  <h5>フロントエンド</h5>
+                  <p>Next.js, React.js, TypeScript, Redux, Zustand</p>
+                </li>
+                <li>
+                  <h5>バックエンド</h5>
+                  <p>Ruby on Rails, Ruby</p>
+                </li>
+                <li>
+                  <h5>インフラ</h5>
+                  <p>AWS, さくらクラウド</p>
+                </li>
+                <li>
+                  <h5>データベース</h5>
+                  <p>Redis, Postgresql</p>
+                </li>
+                <li>
+                  <h5>CI/CD環境</h5>
+                  <p>AWS CodePipeline, AWS Amplify Hosting</p>
+                </li>
+              </ul>
+              <ul className='skill'>
+                <li>
+                  <h5>ソースコード管理</h5>
+                  <p>GitLab, Sourcetree</p>
+                </li>
+                <li>
+                  <h5>タスク管理</h5>
+                  <p>GitLab, Trello</p>
+                </li>
+                <li>
+                  <h5>コンテナ型仮想化プラットフォーム</h5>
+                  <p>Docker, K8s</p>
+                </li>
+                <li>
+                  <h5>開発エディタ</h5>
+                  <p>Cursor, Visual Studio Code</p>
+                </li>
+                <li>
+                  <h5>デザイン</h5>
+                  <p>Adobe illustrator, Adobe Photoshop</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </StyledMain>
@@ -49,14 +90,19 @@ export default function About() {
 
 const StyledMain = styled('main')`
   display: flex;
+  justify-content: center;
+  align-items: start;
   width: 100%;
-  padding-top: 150px;
+  padding: 150px 40px 40px;
+  .img-wrapper {
+    width: 45%;
+    text-align: center;
+  }
   .img {
-    width: 50%;
-    height: auto;
+    border-radius: 50%;
   }
   .contents {
-    width: 50%;
+    padding: 0 4rem;
   }
   .en_name {
     font-size: 24px;
@@ -64,6 +110,30 @@ const StyledMain = styled('main')`
   }
   .bold {
     font-weight: bold;
+  }
+  .skill-wrapper {
+    display: flex;
+    justify-content: start;
+    gap: 10%;
+  }
+  .skill {
+    list-style: circle;
+    margin: 0;
+    padding-left: 1.5rem;
+    > li {
+      :first-child {
+        > h5 {
+          margin-top: 0;
+        }
+      }
+      > h5 {
+        margin: 1rem 0;
+      }
+      > p {
+        margin: 0;
+        padding-left: 0.5rem;
+      }
+    }
   }
   @media screen and (max-width: 768px) {
     display: inherit;
