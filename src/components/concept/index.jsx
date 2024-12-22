2ef555dc5cf81771dc56047694f69d9aa7aced26
kao126 <76607249+kao126@users.noreply.github.com>
2024-12-22 23:51:13 +0900

    style: change styled components into vanilla-extract on Concept comp

---
+++ src/components/concept/index.jsx
@@ -0,0 +1,21 @@
+// Next.js
+import Image from 'next/image';
+
+// styles
+import { styles } from 'src/components/concept/styles.css';
+import conceptImage from 'public/images/CONCEPT.jpg';
+
+export function Concept() {
+  return (
+    <div className={styles.container}>
+      <Image src={conceptImage} alt='コンセプト' className={styles.img} />
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Concept</h1>
+          <p className={styles.description}>ここは、大嶋捷稔（おおしま かなる）の世界観を表現したポートフォリオです。</p>
+          <p className={styles.description}>Welcome to my Portfolio designed by Kanaru Oshima </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/concept/styles.css.ts
@@ -0,0 +1,65 @@
+import { style } from '@vanilla-extract/css';
+
+export const styles = {
+  container: style({
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    backgroundColor: '#333',
+    '@media': {
+      'screen and (min-width: 1024px)': {
+        flexDirection: 'row',
+      },
+    },
+  }),
+  img: style({
+    width: '100%',
+    height: '100dvh',
+    objectFit: 'cover',
+    objectPosition: 'bottom',
+    '@media': {
+      'screen and (min-width: 1024px)': {
+        width: '50%',
+      },
+    },
+  }),
+  contentWrapper: style({
+    width: '100%',
+    alignSelf: 'center',
+    '@media': {
+      'screen and (min-width: 1024px)': {
+        width: '50%',
+      },
+    },
+  }),
+  content: style({
+    color: '#fff',
+    width: '77%',
+    margin: '0 auto 80px',
+    textAlign: 'center',
+    '@media': {
+      'screen and (min-width: 1024px)': {
+        margin: '0 auto 40px',
+      },
+    },
+  }),
+  title: style({
+    paddingBottom: '70px',
+    marginTop: '160px',
+    marginBottom: '50px',
+    '@media': {
+      'screen and (min-width: 1024px)': {
+        marginBottom: '80px',
+      },
+      'screen and (min-width: 428px)': {
+        marginTop: '180px',
+      },
+    },
+  }),
+  description: style({
+    margin: '0',
+    fontSize: '12px',
+    '@media': {
+      'screen and (min-width: 1024px)': {},
+    },
+  }),
+};
